34b91ae71dbbeba4be887b892514d4a37f4009fe
Alex Egger <alex.egger96@gmail.com>
2015-08-23 17:04:49 +0200

    Add login function and template.

--- blog.py
+++ blog.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session, \
+    flash
 
 import pymongo
 
 # Create flask application
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = "Gaggn"
 
 @app.route("/")
 def showPosts():
@@ -22,8 +24,19 @@ def showPosts():
     posts = collection.find()
 
     # Render template with posts.
-    print("Will render posts now.")
     return render_template("posts.html", posts=posts)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != "Test" or request.form['password'] != "Password":
+            error = "Invalid username or password."
+        else:
+            session['loggedIn'] = True
+            flash('You were logged in.')
+            return redirect(url_for('showPosts'))
+    return render_template('login.html', error=error)
 
 if __name__ == "__main__":
     app.run()
