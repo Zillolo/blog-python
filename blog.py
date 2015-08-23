cd41621b965fce01026adee4855b57eae6b04774
Alex Egger <alex.egger96@gmail.com>
2015-08-23 20:41:34 +0200

    Add a function to add new posts from the admin panel.

--- blog.py
+++ blog.py
@@ -20,6 +20,36 @@ def showAdmin():
     flash('You are not authorized to access this site.')
     return redirect(url_for('showPosts'))
 
+@app.route("/admin/posts/add", methods=["POST", "GET"])
+def addPost():
+    if session.get('loggedIn') == True:
+        if request.method == "POST":
+            error = None
+            if not request.form['title']:
+                error = "The title field must be filled."
+            elif not request.form['author']:
+                error = "The author field must be filled."
+            else:
+                # Initialize a connection to the MongoDB.
+                try:
+                    conn = pymongo.MongoClient('localhost', 27017)
+                    print("Connection to MongoDB was successful.")
+                except pymongo.errors.ConnectionFailure as e:
+                    print("Could not connect to MongoDB: %s" % e)
+                    return # Well fuck.
+                db = conn.blog
+                collection = db.posts
+
+                post = {'title' : request.form['title'], 'author' : request.form['author'], 'content' : request.form['content']}
+                collection.insert(post)
+                flash('Post was added successfully.')
+                return redirect(url_for('showPosts'))
+            return render_template('posts/new.html', error=error)
+        else:
+            return render_template('posts/new.html', error=None)
+    flash('You are not authorized to access this site.')
+    return redirect(url_for('showPosts'))
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if session.get('loggedIn') == True:
