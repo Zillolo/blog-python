e02c70221a0d91a68d29152a6f43610b65e481e5
Alex Egger <alex.egger96@gmail.com>
2015-08-24 16:48:35 +0200

    Add a guardian js function for the new and modify forms.

--- static/js/form.js
+++ static/js/form.js
@@ -4,3 +4,29 @@ function showDeleteAlert(url) {
         window.location = url;
     }
 }
+
+function validatePostForm(action) {
+    var errorString = "";
+
+    var title = document.forms["postForm"]["title"].value;
+    if(title == null || title == "") {
+        errorString += "The title field may not be empty.\n";
+    }
+
+    var author = document.forms["postForm"]["author"].value;
+    if(author == null || author == "") {
+        errorString += "The author field may not be empty.\n";
+    }
+
+    var content = document.forms["postForm"]["content"].value;
+    if(content == null || content == "") {
+        errorString += "The content field may not be empty.\n";
+    }
+
+    if(errorString != "") {
+        alert(errorString);
+        return false;
+    } else {
+        return true;
+    }
+}
